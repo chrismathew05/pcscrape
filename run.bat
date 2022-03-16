29199e540626c158acd62f2305d0466e3706bca9
chrismathew05 <chrismathew05@gmail.com>
2022-03-16 16:41:42 -0400

    Check environment in batch script and updated gitignore

--- run.bat
+++ run.bat
@@ -1,6 +1,15 @@
-mkdir venv
-python -m venv venv
-call venv/Scripts/activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+@echo off
+if not exist venv\ (
+    echo Setting up virtual environment and installing requirements...
+    mkdir venv
+    python -m venv venv
+    call venv/Scripts/activate
+    python -m pip install --upgrade pip
+    pip install -r requirements.txt
+
+    echo Running script...
+) else (
+    echo Environment already set up.
+)
+
 python test.py
